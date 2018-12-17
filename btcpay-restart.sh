8000ee6ba5f25af3f04dd1032d7ea9aa937682e2
Claudia Peart <pinklite36@hotmail.com>
2018-12-17 13:09:25 -0500

    merged btcpay with mappum/htlc-restart.sh

--- btcpay-restart.sh
+++ btcpay-restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. /etc/profile.d/btcpay-env.sh
+. /etc/profile.d/mappum/htlc-env.sh
 
-cd "`dirname $BTCPAY_ENV_FILE`" 
-docker-compose -f $BTCPAY_DOCKER_COMPOSE restart -t "${COMPOSE_HTTP_TIMEOUT:-180}"
+cd "`dirname $mappum/htlc_ENV_FILE`" 
+docker-compose -f $mappum/htlc_DOCKER_COMPOSE restart -t "${COMPOSE_HTTP_TIMEOUT:-180}"
